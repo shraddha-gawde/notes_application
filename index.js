9c7b928f51394f0cf62324baa687dd271dee7e96
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-01-10 12:36:43 +0530

    cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const{ connection } = require("./db")
 const{ userRouter } = require("./routes/user.route")
 const{ noteRouter } = require("./routes/notes.route")
+const cors = require("cors")
 const app = express()
 
+app.use(cors)
 app.use(express.json())
 
 app.get("/", (req, res)=>{
