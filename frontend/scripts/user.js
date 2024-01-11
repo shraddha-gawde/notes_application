996945c1f11089764091f5cec180a7c9a2b7afb7
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-01-11 21:44:36 +0530

    done

--- frontend/scripts/user.js
+++ frontend/scripts/user.js
@@ -2,49 +2,38 @@ function getData() {
     fetch("https://long-teal-fossa-wig.cyclic.app/users", {
       headers: {
         "Content-type": "application/json",
-        authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.notes_data);
-        displayNotes(data);
+        console.log(data.user_data);
+        displayUser(data);
       })
       .catch((err) => console.log(err));
   }
   
-  function displayNotes(data) {
-    const noteList = document.getElementById("note-card-list");
-    const notes = data.notes_data;
+  function displayUser(data) {
+    const userList = document.getElementById("user-card-list");
+    const users = data.user_data;
   
-    notes.forEach((note) => {
-      const noteCard = document.createElement("div");
-      noteCard.classList.add("note-card");
+    users.forEach((user) => {
+      const userCard = document.createElement("div");
+      userCard.classList.add("user-card");
   
-      const title = document.createElement("h3");
-      title.innerText = note.title;
+      const name = document.createElement("h4");
+      name.innerText = `username: ${user.username}`;
   
-      const content = document.createElement("p");
-      content.innerText = note.body;
-  
-      const editBtn = document.createElement("button");
-      editBtn.className = "btn1";
-      editBtn.innerText = "Edit";
-      editBtn.addEventListener("click", () => {
-          openEditModal(note._id, note.title, note.body);
-        });
-  
-      const deleteBtn = document.createElement("button");
-      deleteBtn.className = "btn1";
-      deleteBtn.innerText = "Delete";
-      deleteBtn.addEventListener("click", () => {
-          deleteNote(note._id)
-      });
-  
-      noteCard.append(title, content, editBtn, deleteBtn);
+      const email = document.createElement("p");
+      email.innerText = `email: ${user.email}`;
+        
+      const role = document.createElement("p");
+      role.innerText = `role: ${user.role}`;
       
   
-      noteList.appendChild(noteCard);
+      userCard.append(name, email, role);
+      
+  
+      userList.appendChild(userCard);
     });
   }
   
